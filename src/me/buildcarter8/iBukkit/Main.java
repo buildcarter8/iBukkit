393fc7b020a9e45b5ef24e35605cc9fa545ea814
Tyler <buildcarter8@users.noreply.github.com>
2015-07-24 19:23:30 -0400

    Added Spigot Support

--- src/me/buildcarter8/iBukkit/Main.java
+++ src/me/buildcarter8/iBukkit/Main.java
@@ -3,14 +3,16 @@ package me.buildcarter8.iBukkit;
 import java.util.logging.Logger;
 
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-//WARNING: THIS IS ONLY FOR CRAFTBUKKIT IF YOU ARE USING SPIGOT PLEASE DOWNLOAD SPIGOT BUILD
-//Recommended for: CraftBukkit 1.8.6 Can be used for 1.8+
+//Recommended for: CraftBukkit 1.8.* Can be used for: Spigot 1.8.*
 public class Main extends JavaPlugin
 {
 	public static final String iBukkitVersion = "1";
-	public static final String buildNumber = "1";
+	public static final String buildNumber = "2";
+	public static final String iSpigotVersion = "1";
+	public static final String spigotSupported = "git-Spigot-44c59bf-1880a9c";
 	public final Logger log = Logger.getLogger("Minecraft-Server");
 	public static Main plugin;
 	@Override
@@ -20,6 +22,10 @@ public class Main extends JavaPlugin
     @Override
     public void onLoad() {
     	log.info("Loading iBukkit...");
+    	if(Bukkit.getBukkitVersion().equals(spigotSupported))
+    	{
+    		log.info("Seems you are using spigot... not all iBukkit features will be usable");
+    	}
     	log.info("Created by buildcarter8...");
     	log.info("Loading iBukkit Utils...");
     
