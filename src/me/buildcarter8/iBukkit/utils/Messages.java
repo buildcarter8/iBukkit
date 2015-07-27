c1491d3561a9ac3a021fb4c7435a7c3881d8d6d6
Tyler <buildcarter8@users.noreply.github.com>
2015-07-26 22:50:30 -0400

    Update Messages.java

--- src/me/buildcarter8/iBukkit/utils/Messages.java
+++ src/me/buildcarter8/iBukkit/utils/Messages.java
@@ -1,7 +1,9 @@
 package me.buildcarter8.iBukkit.utils;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
 //import org.bukkit.command.CommandSender;
+import org.bukkit.command.CommandSender;
 
 //This util refers to message sending and broadcasting from a plugin made easier
 public class Messages 
@@ -11,14 +13,14 @@ public class Messages
   {
 	  Messages.bcMessage(message, null);
   }
-  //Easier sending a player a message (Temp disabled)
- /*
-  *  public static void playerMsg(CommandSender sender, String message, Color color)
-  *    {
+  //Easier sending a player a message 
+
+  public static void playerMsg(CommandSender sender, String message, Color color)
+     {
     //just type Messages.playerMsg(ChatColor.blah + blah)
       sender.sendMessage(color + message);
   }
-  */
+  
 
   //Error messages
   public static String error = ChatColor.RED + "[iBukkit] Oops! there seems to be an error with that please contact the developer of the plugin with the command used";
