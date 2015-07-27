9f6746b78fc904cd36fdb050ad9bc4ee8f631c24
Tyler <buildcarter8@users.noreply.github.com>
2015-07-26 22:33:15 -0400

    Temp disabled "playerMsg":

--- src/me/buildcarter8/iBukkit/utils/Messages.java
+++ src/me/buildcarter8/iBukkit/utils/Messages.java
@@ -1,7 +1,7 @@
 package me.buildcarter8.iBukkit.utils;
 
 import org.bukkit.ChatColor;
-import org.bukkit.command.CommandSender;
+//import org.bukkit.command.CommandSender;
 
 //This util refers to message sending and broadcasting from a plugin made easier
 public class Messages 
@@ -11,12 +11,15 @@ public class Messages
   {
 	  Messages.bcMessage(message, null);
   }
-  //Easier sending a player a message
-  public static void playerMsg(CommandSender sender, String message, Color color)
-  {
-	  //just type Messages.playerMsg(ChatColor.blah + blah)
+  //Easier sending a player a message (Temp disabled)
+ /*
+  *  public static void playerMsg(CommandSender sender, String message, Color color)
+  *    {
+    //just type Messages.playerMsg(ChatColor.blah + blah)
       sender.sendMessage(color + message);
   }
+  */
+
   //Error messages
   public static String error = ChatColor.RED + "[iBukkit] Oops! there seems to be an error with that please contact the developer of the plugin with the command used";
 }
