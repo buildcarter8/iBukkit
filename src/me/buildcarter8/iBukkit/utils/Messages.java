36f9c3c9a3f6ec35972d1f81d8c283cb0cb83aae
Tyler <buildcarter8@users.noreply.github.com>
2015-07-27 16:25:50 -0400

    Fixed Messages

--- src/me/buildcarter8/iBukkit/utils/Messages.java
+++ src/me/buildcarter8/iBukkit/utils/Messages.java
@@ -1,24 +1,40 @@
 package me.buildcarter8.iBukkit.utils;
 
+
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Color;
 //import org.bukkit.command.CommandSender;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 //This util refers to message sending and broadcasting from a plugin made easier
 public class Messages 
 {
 	//Easier broadcasting a message
-  public static void bcMessage(String message, ChatColor chatcolor)
+    public static void bcMessage(String message, ChatColor color)
+    {
+        for (Player player : Bukkit.getOnlinePlayers())
+        {
+            player.sendMessage((color == null ? "" : color) + message);
+        }
+    }
+  public static void bcMessage(String message)
   {
 	  Messages.bcMessage(message, null);
   }
   //Easier sending a player a message 
 
-  public static void playerMsg(CommandSender sender, String message, Color color)
+  public static void playerMsg(CommandSender sender, String message, Color gray)
      {
     //just type Messages.playerMsg(ChatColor.blah + blah)
-      sender.sendMessage(color + message);
+      sender.sendMessage(gray + message);
+  }
+  public static void playerMsg(CommandSender sender, String message)
+  {
+      Messages.playerMsg(sender, message, Color.WHITE);
   }
   
 
