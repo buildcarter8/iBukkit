bb899dc81831347705f89b4ea893d2c1626d69c9
Tyler <buildcarter8@users.noreply.github.com>
2015-07-30 12:13:00 -0400

    Update Main.java

--- src/me/buildcarter8/iBukkit/Main.java
+++ src/me/buildcarter8/iBukkit/Main.java
@@ -1,12 +1,17 @@
 package me.buildcarter8.iBukkit;
 
 import java.util.logging.Logger;
+
+import me.buildcarter8.iBukkit.listeners.PlayerLogin;
+import me.buildcarter8.iBukkit.utils.Ips;
 import me.buildcarter8.iBukkit.utils.Messages;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 //Recommended for: CraftBukkit 1.8.* Can be used for: Spigot 1.8.6
@@ -33,6 +38,8 @@ public class Main extends JavaPlugin
     @Override
     public void onEnable() {
     	log.info("iBukkit Features are now usable!");
+    	final PluginManager pm = Bukkit.getServer().getPluginManager();
+   	 pm.registerEvents(new PlayerLogin(), this);
     }
     //TODO: Code it better
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
@@ -42,8 +49,30 @@ public class Main extends JavaPlugin
 			 sender.sendMessage(ChatColor.AQUA + "iBukkit -- Version " + iBukkitVersion + "." + buildNumber);
 			 sender.sendMessage(ChatColor.AQUA + "iBukkit was designed to help the average developer and make it easier for him/her");
 		 }
+		 else if(cmd.getName().equalsIgnoreCase("getIp"))
+		 {
+			 if(sender.isOp())
+			 {
+			 if (args.length == 0)
+			 {
+			 sender.sendMessage(ChatColor.RED + "Your ip is: " + Ips.getIp(player));
+			 }
+			 else if (args.length == 1) {
+			        if (player.getServer().getPlayer(args[0]) != null) {
+			          Player targetPlayer = player.getServer().getPlayer(args[0]);
+			          sender.sendMessage(ChatColor.RED + targetPlayer.getName() + "'s ip is: " + Ips.getIp(targetPlayer));
+			        }
+			 }
+			        else
+			        	sender.sendMessage(ChatColor.RED + "NOPE");
+			 
+		 }
 		return true;
 		 
     }
+		return true;
 		 
+}
+    {
+}
 }
