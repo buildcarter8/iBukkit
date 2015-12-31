a787890a683472b52dd24ac9e05a7eadc35467e9
BabyBreezy <tylerhuffaker02@gmail.com>
2015-12-31 17:13:35 -0500

    Create Colors.java

---
+++ src/me/buildcarter8/iBukkit/utils/Colors.java
@@ -0,0 +1,33 @@
+package me.buildcarter8.iBukkit.utils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import org.bukkit.ChatColor;
+
+public class Colors 
+{
+
+	private static final Random RANDOM = new Random();
+	public static final List<ChatColor> CHAT_COLOR_POOL = Arrays.asList(
+            ChatColor.DARK_BLUE,
+            ChatColor.DARK_GREEN,
+            ChatColor.DARK_AQUA,
+            ChatColor.DARK_RED,
+            ChatColor.DARK_PURPLE,
+            ChatColor.GOLD,
+            ChatColor.BLUE,
+            ChatColor.GREEN,
+            ChatColor.AQUA,
+            ChatColor.RED,
+            ChatColor.LIGHT_PURPLE,
+            ChatColor.YELLOW);
+	//Allows random chat colors
+    public static ChatColor randChatColor()
+    {
+        return CHAT_COLOR_POOL.get(RANDOM.nextInt(CHAT_COLOR_POOL.size()));
+    }
+    
+    
+}
